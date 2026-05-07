9e473884e9376be66d5e5dc6073f24262acb25a9
Mythryl <noah.philipp@gmail.com>
2026-05-07 20:05:15 +0200

    Added Button B and AB support aswell as 50°C graphic
    After spending a few hours  i have now successfully 
    reverse engineered my own code from 7 years ago wich i sadly lost the source code of. 
    
    With this Version the Thermometer gain the Ability back to be muted and unmuted as well as a proper death Screen if the Temperature reaches 50°C

--- JavaScript_Version/Thermometer.js
+++ JavaScript_Version/Thermometer.js
@@ -1,72 +1,74 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
+input.onButtonPressed(Button.A, function () {
     pause2 = 1
     basic.clearScreen()
-    basic.showString("" + ("" + input.temperature()))
+    basic.showString("" + input.temperature())
     pause2 = 0
+})
+input.onButtonPressed(Button.AB, function () {
+    music.setBuiltInSpeakerEnabled(true)
+    pause2 = 0
+    music.setVolume(75)
+})
+input.onButtonPressed(Button.B, function () {
+    music.setBuiltInSpeakerEnabled(false)
+    pause2 = 0
+    music.setVolume(0)
 })
 let repeat = 0
 let repeat_2 = 0
 let pause2 = 0
-music.setBuiltInSpeakerEnabled(false)
+music.setBuiltInSpeakerEnabled(true)
 pause2 = 0
 music.setVolume(75)
-basic.forever(function on_forever() {
+basic.forever(function () {
+    if (pause2 == 0) {
+        if (input.temperature() == 50) {
+            basic.showIcon(IconNames.Skull)
+        } else {
+            led.plotBarGraph(
+                input.temperature(),
+                             50
+            )
+        }
+    }
+})
+basic.forever(function () {
     if (input.temperature() == 50) {
         basic.showIcon(IconNames.Skull)
         for (let index = 0; index < 4; index++) {
             music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
         }
     }
-    
 })
-basic.forever(function on_forever2() {
-    if (pause2 == 0) {
-        if (input.temperature() == 50) {
-            basic.showIcon(IconNames.Skull)
-        } else {
-            led.plotBarGraph(input.temperature(), 50)
-        }
-        
-    }
-    
-})
-basic.forever(function on_forever3() {
-    
+basic.forever(function () {
     if (input.temperature() >= 30 && input.temperature() <= 40) {
-        for (let index2 = 0; index2 < repeat_2; index2++) {
+        for (let index = 0; index < repeat_2; index++) {
             music.playMelody("F D E F A D F E ", 120)
         }
     }
-    
     if (input.temperature() >= 30 && input.temperature() <= 40) {
         repeat_2 += 1
     } else {
         repeat_2 = 0
     }
-    
 })
-basic.forever(function on_forever4() {
+basic.forever(function () {
     if (input.temperature() >= 30 && input.temperature() <= 40) {
         pins.analogWritePin(AnalogPin.P1, 1023)
     } else {
         pins.analogWritePin(AnalogPin.P1, 0)
     }
-    
 })
-basic.forever(function on_forever5() {
-    
+basic.forever(function () {
     if (input.temperature() >= 40 && input.temperature() <= 49) {
         music.setVolume(255)
-        for (let index3 = 0; index3 < repeat; index3++) {
+        for (let index = 0; index < repeat; index++) {
             music.playMelody("C5 B C5 B C5 B C5 B ", 500)
         }
     }
-    
     if (input.temperature() >= 40 && input.temperature() <= 49) {
         repeat += 1
     } else {
         repeat = 0
     }
-    
 })
